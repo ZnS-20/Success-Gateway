644c81d3039b6861f7d4b31cf133133635009589
vipin katara <vipinkatara2@gmail.com>
2021-10-08 17:21:58 +0530

    Leetcode 513 added

---
+++ LeetCode/Tree/Solution513.cpp
@@ -0,0 +1,43 @@
+// 513. Find Bottom Left Tree Value
+/
+// Given the root of a binary tree, return the leftmost value in the last row of the tree.
+
+// Example 1:
+// Input: root = [2,1,3]
+// Output: 1
+
+// Example 2:
+// Input: root = [1,2,3,4,null,5,6,null,null,7]
+// Output: 7
+ 
+// Constraints:
+// The number of nodes in the tree is in the range [1, 104].
+// -231 <= Node.val <= 231 - 1
+
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        if(!root)
+            return -1;
+        vector<int> vec;
+        int val = root->val;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        while(q.size() > 1){
+            TreeNode* curr = q.front();
+            q.pop();
+            if(!curr){
+                val = q.front()->val;
+                q.push(NULL);
+                continue;
+            }
+            if(curr->left)
+                q.push(curr->left);
+            if(curr->right)
+                q.push(curr->right);
+            
+        }
+        return val;
+    }
+};
